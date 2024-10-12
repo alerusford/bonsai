4f9bd7be124e8953cbc8a931991cbadb2656b717
Alexandr <alerusford@gmail.com>
2024-10-12 12:05:30 +0300

    calibration sensor YL-69

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,14 +2,14 @@
 #include <LiquidCrystal_I2C.h>
 #include "DHT.h"
 
-#define DHTPIN A2 // порт куда подключен сигнальный выход датчика температуры
-#define DHTTYPE DHT11 // DHT 11
-#define analogHumidityPin A1 // порт для подключения аналогового датчика влажности почвы YL-69
+#define DHTPIN A2 // Пин подключения сигнального выхода датчика температуры
+#define DHTTYPE DHT11 // DHT-11
+#define analogHumidityPin A1 // Порт для подключения аналогового датчика влажности почвы
 #define analogHumidityPower 8 // Определяем пин подачи питания на датчик YL-69
 
-// инициализация датчика
+// Инициализация датчика DHT-11
 DHT dht(DHTPIN, DHTTYPE);
-byte degree[8] = // кодируем символ градуса
+byte degree[8] = // Кодируем символ градуса
 {
   B00111,
   B00101,
@@ -29,37 +29,25 @@ void setup()
   lcd.createChar(1, degree); // Создаем символ под номером 1
   pinMode(analogHumidityPower, OUTPUT);  // Определяем пин питания датчика влажности почвы, как выход
   Serial.begin(9600); // Инициализируем серийный порт как устройство вывода
-  dht.begin();
+  dht.begin(); // Инициализация DHT-11
 }
 
 void loop() {
-  delay(10000); // Добавляем паузы в несколько секунд между измерениями
-  
-  // Чтение влажности и температуры с DHT
+  delay(30000); // Добавляем паузы между измерениями
+
+  // Чтение влажности и температуры с DHT-11
   int h = dht.readHumidity();
   int t = dht.readTemperature();
-  // Serial.println(h);  
-  // Serial.println(t);  
-  
-  // // Чтение аналогового значения с датчика влажности почвы
-  // // uint16_t sensor;
-  // int sensor_signal = 0;
-  // int sensor_procent = 0;
-  // sensor_signal = analogRead(analogHumidityPin); 
-  // // Serial.println(sensor);  
-  // // Преобразование аналогового значения в процент (предположим, что это соответствует диапазону)
-  // // float analogHumidityPercentage = (analogHumidityValue / 100); // Преобразуем в проценты (0-100%)
-  // // sensor_procent = map(sensor_signal, 1019, 244, 0, 100); // РАБОЧИЙ ВАРИАНТ
-  // sensor_procent = map(sensor_signal, 100, 1023, 100, 0); // ВАРИАНТ 2
-  // // Serial.println(sensor_procent);
+  // Serial.println(h);
+  // Serial.println(t);
 
   // Чтение аналогового значения с датчика влажности почвы / версия 2
+  int very_moist_value = 46; // Показания датчика (analogHumiditySignal) при очень мокрой почве (для калибровки процентного вывода)
   digitalWrite(analogHumidityPower, HIGH); // Включаем питание на пине, к которому подключен плюс датчика
-  delay(100); // Время, необходимое датчику, чтобы включиться и замерить показания
+  delay(300); // Время, необходимое датчику, чтобы включиться и замерить показания
   int analogHumiditySignal = analogRead(analogHumidityPin); // Считываем показания датчика YL-69 (от 0 до 1023, где 1023 - сухо)
   digitalWrite(analogHumidityPower, LOW);  // Отключаем питание на пине, к которому подключен плюс датчика
-  int very_moist_value = 100; // Показания датчика (analogHumiditySignal) при очень мокрой почве (для калибровки процентного вывода)
-  int analogHumiditySignalPercent = map(analogHumiditySignal, very_moist_value, 1023, 100, 0);  // Переводим показания датчика YL-69 в проценты 
+  int analogHumiditySignalPercent = map(analogHumiditySignal, very_moist_value, 1023, 100, 0);  // Переводим показания датчика YL-69 в проценты
   Serial.print(String(analogHumiditySignal) + " = "); // Выводим показания датчика в строковом формате на устройство вывода
   Serial.println(String(analogHumiditySignalPercent) + "%"); // Выводим показания о влажности в процентах в строковом формате на устройство вывода
 
@@ -72,18 +60,14 @@ void loop() {
 
   lcd.setCursor(11, 0); // Устанавливаем курсор
   lcd.print(t,1); // Выводим значение температуры
-  lcd.print("\1"); // знак градуса
+  lcd.print("\1"); // Знак градуса
 
   lcd.setCursor(0, 1); // Устанавливаем курсор в начало второй строки
   lcd.print("Hum ="); // Выводим текст
   lcd.setCursor(6, 1); // Устанавливаем курсор
-
-  // lcd.print(sensor_procent, 1); // Выводим процент влажности с аналогового датчика
   lcd.print(analogHumiditySignalPercent, 1); // Выводим процент влажности с аналогового датчика
-
   lcd.print("% ");
-  lcd.setCursor(11, 1); // Устанавливаем курсор
-  // lcd.print(sensor_signal, 1); // Выводим значение влажности с аналогового датчика
-  lcd.print(analogHumiditySignal, 1); // Выводим значение влажности с аналогового датчика
 
+  lcd.setCursor(11, 1); // Устанавливаем курсор
+  lcd.print(analogHumiditySignal, 1); // Выводим значение влажности с аналогового датчика
 }
